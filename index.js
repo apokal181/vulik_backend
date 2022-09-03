dacb162ea24ac7b62060b8a92053dfd5ae5003df
Vadim Chervonchenko <vadim.dev27@gmail.com>
2022-09-04 02:27:53 +0300

    db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const Sequelize = require('sequelize')
-
+const models = require('./models');
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -25,26 +25,29 @@ app.use('/api', router)
 // крайний мидлвариина обрбаботка ошибок, после него никаких роутов ничего !
 app.use(errorMiddleware);
 
-const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env
-
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-    host: 'ec2-44-209-186-51.compute-1.amazonaws.com',
-    dialect: 'postgres',
-    logging: false,
-    ...(process.env.NODE_ENV === 'production' && {
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-})})
-
+// const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env
+//
+// const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+//     host: 'ec2-44-209-186-51.compute-1.amazonaws.com',
+//     dialect: 'postgres',
+//     logging: false,
+//     ...(process.env.NODE_ENV === 'production' && {
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false,
+//             },
+//         },
+// })})
 const start = async () => {
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        // await sequelize.authenticate()
+        models.sequelize.sync({force: true}).then(function() {
+            // Start the express server listening once the sequelize sync operation has completed
+            app.listen(PORT, function() {
+                console.log('Node app is running on port', PORT);
+            });
+        });
     } catch (error) {
         console.error('Unable to connect to the database:', error)
     }
